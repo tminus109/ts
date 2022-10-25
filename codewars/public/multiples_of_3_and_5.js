1a0b4e8747e35f60c420c5d7047eb1f0b6b860e0
tminus109 <martontibor123@gmail.com>
2022-10-25 13:55:45 +0200

    refact: for loop starts with 3

--- codewars/public/multiples_of_3_and_5.js
+++ codewars/public/multiples_of_3_and_5.js
@@ -1,10 +1,10 @@
 "use strict";
 function findMultiples(input) {
-    if (input < 0 || input === 0) {
+    if (input < 3) {
         return 0;
     }
     let sum = 0;
-    for (let i = 0; i < input; i++) {
+    for (let i = 3; i < input; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
             sum += i;
         }
--- codewars/src/multiples_of_3_and_5.ts
+++ codewars/src/multiples_of_3_and_5.ts
@@ -1,11 +1,11 @@
 function findMultiples(input: number): number {
-  if (input < 0 || input === 0) {
+  if (input < 3) {
     return 0;
   }
 
   let sum = 0;
 
-  for (let i = 0; i < input; i++) {
+  for (let i = 3; i < input; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     }
