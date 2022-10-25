dd0369d0784ebd74aeafa9ac6666aaa4a5b9ef74
tminus109 <martontibor123@gmail.com>
2022-10-25 13:30:09 +0200

    Most Profit from Stock Quotes exercise complete

---
+++ codewars/src/most_profit_from_stock_quotes.ts
@@ -0,0 +1,29 @@
+function countMostProfit(quotes: number[]): number {
+  let profit = 0;
+  let stockCount = 0;
+
+  for (let i = 0; i < quotes.length; i++) {
+    let bestPrice = quotes[i];
+
+    for (let j = i + 1; j < quotes.length; j++) {
+      if (quotes[j] > bestPrice) {
+        bestPrice = quotes[j];
+      }
+    }
+
+    if (bestPrice > quotes[i]) {
+      stockCount++;
+      profit -= quotes[i];
+    } else if (bestPrice == quotes[i]) {
+      profit += stockCount * bestPrice;
+      stockCount = 0;
+    }
+  }
+
+  return profit;
+}
+
+console.log(countMostProfit([1, 2, 3, 4, 5, 6]));
+console.log(countMostProfit([6, 5, 4, 3, 2, 1]));
+console.log(countMostProfit([1, 6, 5, 10, 8, 7]));
+console.log(countMostProfit([1, 2, 10, 3, 2, 7, 3, 2]));
