e41397f3c095018ec3661e76064439ee333c83c2
tminus109 <martontibor123@gmail.com>
2022-10-26 20:00:38 +0200

    Birthday Cake Candles exercise complete

---
+++ hacker_rank/src/birthday_cake_candles.ts
@@ -0,0 +1,21 @@
+function countTallestCandles(candles: number[]): number {
+  let tallestCandleCount = 1;
+  let tallestCandle = candles[0];
+
+  for (let i = 1, l = candles.length; i < l; i++) {
+    const candle = candles[i];
+    if (candle > tallestCandle) {
+      tallestCandle = candle;
+      tallestCandleCount = 1;
+    } else if (candle === tallestCandle) {
+      tallestCandleCount++;
+    }
+  }
+
+  return tallestCandleCount;
+}
+
+console.log(countTallestCandles([4, 4, 1, 3]));
+console.log(countTallestCandles([3, 2, 1, 3]));
+console.log(countTallestCandles([3, 3, 3, 4, 5, 7, 6, 4, 2, 8, 4, 4, 8, 8, 8]));
+console.log(countTallestCandles([3, 3, 4, 5, 7, 6, 4, 2, 8, 4, 4, 8, 8, 9, 8]));
