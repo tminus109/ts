80e401c6acef85736ad3efad491895c24d73a610
tminus109 <martontibor123@gmail.com>
2022-10-26 15:37:13 +0200

    Valid Parenthesis exercise complete

---
+++ codewars/src/valid_parenthesis.ts
@@ -0,0 +1,37 @@
+function isParenthesisValid(input: string): boolean {
+  if (input === "") {
+    return false;
+  }
+
+  const bracketsOnly = input.replaceAll(/[^()]/g, "");
+  const length = bracketsOnly.length;
+
+  if (
+    bracketsOnly === "" ||
+    bracketsOnly.charAt(0) !== "(" ||
+    bracketsOnly.charAt(length - 1) !== ")"
+  ) {
+    return false;
+  }
+
+  let count = 0;
+
+  for (let i = 0; i < length; i++) {
+    if (bracketsOnly.charAt(i) === "(") {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  return count === 0;
+}
+
+console.log(isParenthesisValid("baba()"));
+console.log(isParenthesisValid(")(()))"));
+console.log(isParenthesisValid("((()))("));
+console.log(isParenthesisValid("(())((()())())"));
+console.log(isParenthesisValid("("));
+console.log(isParenthesisValid(")"));
+console.log(isParenthesisValid(""));
+console.log(isParenthesisValid("baba"));
