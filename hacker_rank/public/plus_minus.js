bb6e809c6079baadd0963e0efd8b1dd32ab89ed7
tminus109 <martontibor123@gmail.com>
2022-10-27 14:55:14 +0200

    Plus Minus exercise complete

---
+++ hacker_rank/public/plus_minus.js
@@ -0,0 +1,23 @@
+"use strict";
+function calcRatios(arr) {
+    const length = arr.length;
+    let posCount = 0;
+    let negCount = 0;
+    let zeroCount = 0;
+    for (const n of arr) {
+        if (n > 0) {
+            posCount++;
+        }
+        else if (n < 0) {
+            negCount++;
+        }
+        else {
+            zeroCount++;
+        }
+    }
+    console.log("pos: " + (posCount / length).toFixed(6));
+    console.log("neg: " + (negCount / length).toFixed(6));
+    console.log("zero: " + (zeroCount / length).toFixed(6));
+}
+calcRatios([1, 1, 0, -1, -1]);
+calcRatios([-4, 3, -9, 0, 4, 1]);
