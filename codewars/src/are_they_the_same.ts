7bcf952ab425dc4362c8b41f51d668709f159963
tminus109 <martontibor123@gmail.com>
2022-11-08 10:45:47 +0100

    fix: updated sort methods

--- codewars/src/are_they_the_same.ts
+++ codewars/src/are_they_the_same.ts
@@ -2,13 +2,21 @@ function comp(a: Array<number> | null, b: Array<number> | null): boolean {
   if (a === null || b === null || a.length !== b.length) {
     return false;
   }
-  a.sort();
-  b.sort();
+
+  a.sort((a, b) => {
+    return a - b;
+  });
+
+  b.sort((a, b) => {
+    return a - b;
+  });
+
   for (let i = 0, l = a.length; i < l; i++) {
     if (b[i] !== Math.pow(a[i], 2)) {
       return false;
     }
   }
+
   return true;
 }
 
@@ -29,8 +37,14 @@ const a3 = [121, 144, 19, 161, 19, 144, 19, 11];
 const b3 = [132, 14641, 20736, 361, 25921, 361, 20736, 361];
 const a4 = [121, 144, 19, 161, 19, 144, 19, 11];
 const b4 = [121, 14641, 20736, 36100, 25921, 361, 20736, 361];
+const a5 = [41, 73, 23, 80, 29, 54, 52, 41, 8, 17, 75, 24, 80, 0, 47, 96, 16];
+const b5 = [
+  1681, 5329, 529, 6400, 841, 2916, 2704, 1681, 64, 289, 5625, 576, 6400, 0,
+  2209, 9216, 256,
+];
 
 console.log(comp(a1, b1));
 console.log(comp(a2, b2));
 console.log(comp(a3, b3));
 console.log(comp(a4, b4));
+console.log(comp(a5, b5));
