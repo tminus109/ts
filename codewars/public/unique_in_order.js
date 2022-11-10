137b07dba42c829372d503ebebd3b6ec8e2a135b
tminus109 <martontibor123@gmail.com>
2022-11-10 13:16:22 +0100

    refact: replaced solution with .filter()

--- codewars/public/unique_in_order.js
+++ codewars/public/unique_in_order.js
@@ -1,25 +1,8 @@
 "use strict";
 function getUniqueInOrder(input) {
-    const uniqueInOrder = [];
-    const l = input.length - 1;
-    if (typeof input === "string") {
-        uniqueInOrder.push(input.charAt(0));
-        for (let i = 0; i < l; i++) {
-            if (input.charAt(i + 1) !== input.charAt(i)) {
-                uniqueInOrder.push(input.charAt(i + 1));
-            }
-        }
-    }
-    else {
-        uniqueInOrder.push(input[0]);
-        for (let j = 0; j < l; j++) {
-            if (input[j + 1] !== input[j]) {
-                uniqueInOrder.push(input[j + 1]);
-            }
-        }
-    }
-    return uniqueInOrder;
+    return [...input].filter((currentValue, index) => currentValue !== input[index - 1]);
 }
 console.log(getUniqueInOrder("AAAABBBCCDAABBB"));
 console.log(getUniqueInOrder("ABBCcAD"));
 console.log(getUniqueInOrder([1, 2, 2, 3, 3]));
+console.log(getUniqueInOrder([]));
--- codewars/src/unique_in_order.ts
+++ codewars/src/unique_in_order.ts
@@ -1,26 +1,10 @@
 function getUniqueInOrder(input: string | number[]): string[] | number[] {
-  const uniqueInOrder: any[] = [];
-  const l = input.length - 1;
-
-  if (typeof input === "string") {
-    uniqueInOrder.push(input.charAt(0));
-    for (let i = 0; i < l; i++) {
-      if (input.charAt(i + 1) !== input.charAt(i)) {
-        uniqueInOrder.push(input.charAt(i + 1));
-      }
-    }
-  } else {
-    uniqueInOrder.push(input[0]);
-    for (let j = 0; j < l; j++) {
-      if (input[j + 1] !== input[j]) {
-        uniqueInOrder.push(input[j + 1]);
-      }
-    }
-  }
-
-  return uniqueInOrder;
+  return [...input].filter(
+    (currentValue, index) => currentValue !== input[index - 1]
+  ) as string[] | number[];
 }
 
 console.log(getUniqueInOrder("AAAABBBCCDAABBB"));
 console.log(getUniqueInOrder("ABBCcAD"));
 console.log(getUniqueInOrder([1, 2, 2, 3, 3]));
+console.log(getUniqueInOrder([]));
