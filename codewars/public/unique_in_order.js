020177c36c89a1c777f3ccd258a9ea375364cb76
tminus109 <martontibor123@gmail.com>
2022-12-08 11:11:23 +0100

    cleanup: untracked public folders

--- codewars/public/unique_in_order.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function getUniqueInOrder(input) {
-    return [...input].filter((currentValue, index) => currentValue !== input[index - 1]);
-}
-console.log(getUniqueInOrder("AAAABBBCCDAABBB"));
-console.log(getUniqueInOrder("ABBCcAD"));
-console.log(getUniqueInOrder([1, 2, 2, 3, 3]));
-console.log(getUniqueInOrder([]));
